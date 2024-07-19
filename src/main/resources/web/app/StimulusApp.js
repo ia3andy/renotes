a5a71134f649e8a2835ae9e23d98771995639cd1
Andy Damevin <ia3andy@gmail.com>
2024-07-19 13:26:22 +0200

    Update versions

--- src/main/resources/web/app/StimulusApp.js
+++ src/main/resources/web/app/StimulusApp.js
@@ -1,6 +1,3 @@
-import "htmx.org"
-import "bootstrap/scss/bootstrap.scss";
-import "bootstrap-icons/font/bootstrap-icons.scss";
 import { Application } from "@hotwired/stimulus";
 
 const StimulusApp = Application.start();
